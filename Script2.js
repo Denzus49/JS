4a3f670109baea9975e1809b1b2d1333e245130c
Dzianis Yarash <den2041@rabmbler.ru>
2024-01-28 11:01:08 +0300

    3

--- Script2.js
+++ Script2.js
@@ -1,6 +1,3 @@
-let byte=(10**1);
-let kilo=(byte**3);
-let mega= (byte**6);
-let giga= (mega*1024);
-let tera= (giga*1024);
-alert (mega);
+let f = 541;
+let c = (f-32) *(5/9);
+alert (c.toFixed(3));
