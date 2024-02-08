e7f9475907e782ee84629282addc81e613733a08
astonuser <den2041@gmail.com>
2024-02-08 14:42:40 +0300

    08.02

--- Конвертер для зп.js
+++ Конвертер для зп.js
@@ -1,7 +1,17 @@
 let brutt = {x: 216406, y: 13}
 let proc= (brutt.x/100*brutt.y)
 let nettoRu= (brutt.x-proc)
-let cource= 0.0354
-let nettoBy= (nettoRu*cource).toFixed(2)
-
+async function fetchCurrencyRate() {
+    try {
+      const response = await fetch('https://api.nbrb.by');
+  
+      const data = await response.json();
+      const currencyRate = data.Cur_QuotName; 
+      console.log('Текущий курс валюты:', cource);
+    } catch (error) {
+      console.error('Произошла ошибка:', error);
+    }
+  }
+  fetchCurrencyRate();
+  let nettoBy= (nettoRu*cource).toFixed(2)
 alert(nettoBy);
