f217a68cb906a49644099902fefe0e62a2f08001
astonuser <den2041@gmail.com>
2024-02-12 11:56:38 +0300

    129 page

--- Operators.js
+++ Operators.js
@@ -1,14 +1,15 @@
-let day = 14
-if (day>0 && day<=10) {alert('1 decade')
-	
-} else if (day>10 && day<=20) {alert('2 decade')
-	
-} else if (day>20 && day<=31) {alert('3 decade')
-	
-} else {alert('Failed')} 
-{
-	
-} {
-	
+let lang = 'en';
+
+switch (lang){
+	case 'ru':
+		console.log('рус');
+		break
+		case 'en':
+			console.log ('англ')
+			break
+		case 'de':
+			console.log ('нем')
+			break
+		default:
+			console.log ('язык не найден')
 }
-	
