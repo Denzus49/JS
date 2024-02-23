ccbc63481ab9e075e2e02b2da3ab82c791d2a3e9
Dzianis Yarash <den2041@rabmbler.ru>
2024-02-23 14:03:13 +0300

    138 page

--- Operators.js
+++ Operators.js
@@ -1,4 +1,6 @@
-let a = 2 ** 4;
-let b = 4 ** 2;
-
-console.log(a != b);
+let arr = [1,2,3]
+let sum = 1
+if (arr.length == 2){sum= arr[0]+arr[1]+arr[2]
+}
+else {console.log ('ничего ты не знаешь джон сноу')}
+console.log (sum)
