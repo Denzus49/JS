a515ea010eafeb868f7ac16dbb0141a939c03c7b
astonuser <den2041@gmail.com>
2024-03-06 17:33:23 +0300

    143 Введение в циклы

--- Operators.js
+++ Operators.js
@@ -1,6 +1,6 @@
-let req = confirm("Вам исполнилось 18 лет?");
-if (req) {alert('Взрослый ответ')
+let str = '143115'
+if (Number(str[0])+Number(str[1])+Number(str[2]) == Number(str[3])+Number(str[4])+Number(str[5])) {console.log ('da')
     
-} else {alert('Вы маленький, доступ закрыт')
+} else { console.log('net')
     
 }
